fb2e38bff4ee62a028bc4151bf2a54c2488b0c7a
septiananugrahh <septiananugrahh@gmail.com>
2026-01-17 19:54:17 +0700

    fix tabel width checklist penilaian [deploy]

--- app/Views/admin/pdf/checklist_pdf_template.php
+++ app/Views/admin/pdf/checklist_pdf_template.php
@@ -228,11 +228,11 @@
         <table class="table-checklist" style="width:100%;">
           <?php
           // Pengaturan lebar kolom
-          $lebar_tujuan = '20%';
+          $lebar_tujuan = '23%';
           $lebar_sudah_muncul = '5%';
-          $lebar_konteks = '25%';
-          $lebar_tempat_waktu = '25%';
-          $lebar_kejadian = '25%';
+          $lebar_konteks = '20%';
+          $lebar_tempat_waktu = '20%';
+          $lebar_kejadian = '32%';
           ?>
 
           <colgroup>
